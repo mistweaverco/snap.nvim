14a88514e11c402346d6ddf7f9f0d56ac9b2b86a
Marco Kellershoff <marco@mistweaverco.com>
2025-12-20 13:41:19 +0100

    wip

--- lua/snap/view.lua
+++ lua/snap/view.lua
@@ -13,6 +13,9 @@ function M.scroll_into_view(winnr, row, col, range)
   -- if in view, no need to scroll
   if row >= (nvim_cursor[1] - 1) and row < (nvim_cursor[1] - 1 + height) then
     vim.cmd("redraw")
+    -- Wait for redraw to complete and highlights to be computed
+    -- This is a blocking call to ensure highlights are ready
+    vim.uv.sleep(0.05) -- 5ms delay to ensure highlights are computed
     return
   end
   if range then
@@ -20,12 +23,19 @@ function M.scroll_into_view(winnr, row, col, range)
     -- if in view, no need to scroll
     if range.start_line >= (nvim_cursor[1] - 1) and range.end_line < (nvim_cursor[1] - 1 + height) then
       vim.cmd("redraw")
+      -- Wait for redraw to complete and highlights to be computed
+      -- This is a blocking call to ensure highlights are ready
+      vim.uv.sleep(0.05) -- 5ms delay to ensure highlights are computed
       return
     end
   end
   -- scroll so that row is the top most line
   vim.api.nvim_win_set_cursor(winnr, { row + 1, col })
   vim.cmd("redraw")
+  -- Wait for redraw to complete and highlights to be computed
+  -- This is critical: highlights need time to be computed after scrolling
+  -- This is a blocking call to ensure highlights are ready before inspection
+  vim.uv.sleep(0.05) -- 5ms delay to ensure highlights are computed
 end
 
 ---Scroll back if at last row of the view
